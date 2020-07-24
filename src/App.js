c14688dfe06d918949ef9b5c298f38dbaa830ac0
shad <shadrais321@outlook.com>
2020-07-25 00:01:42 +0530

    Logo Updated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navigation from "./Components/Navigation/Navigation";
+import Logo from "./Components/Logo/Logo";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Navigation.css';
+
+
+const Navigation = () => {
+    return  (
+        <nav>
+            <p className= 'f3 link dim underline black pa3 pointer'>
+                {'Sign Out'}
+            </p>
+        </nav>
+    )
+}
+
+export default Navigation;
