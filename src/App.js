9fcfb5778f5026e5a0dca49933ff16b29b315357
shad <shadrais321@outlook.com>
2020-07-25 17:01:04 +0530

    Input and Button added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
+import Imagelinkfom from './Components/ImageLinkForm/Imagelinkform'
+
+const particleOptions = {
+    particles: {
+        line_linked: {
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+            }
+        }
+    }
+}
 
 function App() {
   return (
     <div className="App">
+        <Particles
+            params={particleOptions}
+        />
       <Navigation />
       <Logo />
+      <Imagelinkfom />
     </div>
   );
 }
