edee54331f822259a3d1beb99051df74864bc63e
shad <shadrais321@outlook.com>
2020-07-26 15:08:13 +0530

    Added Particles

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Rank = () => {
+    return(
+        <div>
+            <div className='f3 white'>
+                {'Shad, Your Current Rank is'}
+            </div>
+            <div className='f2 white'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Rank;
